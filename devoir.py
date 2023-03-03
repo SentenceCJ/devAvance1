468aab0441114f34ba650c76fd5555d4f88495df
Antoine <antoine.macheda@ynov.com>
2023-03-03 09:51:31 +0100

    First

---
+++ devoir.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+# Ask user the price for a unity
+Price = float(input("Enter the price : "))
+
+# Ask user a quantity
+quantity = int(input("Enter a quantity : "))
+
+# Calculer total
+total = Price * quantity
+
+# Print total
+print("For", quantity, "item the price is",  total, "$")
